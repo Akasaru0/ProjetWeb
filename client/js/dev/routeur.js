44342485b01f4b02f396d6a241d3ccff630c9acd
LaymajouxClement <laymajouxclement@gmail.com>
2023-03-07 00:50:59 +0100

    liste salle / update all client-side

--- client/js/dev/routeur.js
+++ client/js/dev/routeur.js
@@ -1,24 +1,31 @@
-page = "menu.html"
-$(document).ready(function(){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', generateURL(page), true);
-    // $("#content").load(generateURL(page));
-    xhr.responseType = 'document';
-
-    // handle the response
-    xhr.onload = function() {
-    if (xhr.status === 200) {
-        // get the HTML document from the response
-        var htmlDocument = xhr.response.documentElement.innerHTML;
-        console.log(htmlDocument);
-        // insert the content into the page
-        document.getElementById('content').innerHTML = htmlDocument;
+export class Routeur{
+    constructor(){
+        this.baseRouteur = "/client/html/";
     }
-    };
-    // send the request
-    xhr.send();
-});
 
-function generateURL(page){
-    return "html/"+page;
+    routerRequete(page){
+        return this.baseRouteur + page + "?t=" + new Date().getTime(); 
+    }
+
+    includeHtmlFile(page="menu.html",initFunc=false){
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', this.routerRequete(page), true);
+
+        xhr.responseType = 'document';
+
+        // handle the response
+        xhr.onload = function() {
+        if (xhr.status === 200) {
+            // get the HTML document from the response
+            var htmlDocument = xhr.response.documentElement.innerHTML;
+            // insert the content into the page
+            document.getElementById('content').innerHTML = htmlDocument;
+            if(initFunc){
+                initFunc();
+            }
+        }
+        };
+        // send the request
+        xhr.send();
+    }   
 }

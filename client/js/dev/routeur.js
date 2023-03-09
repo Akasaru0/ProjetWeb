7896457c47abeff7048b118bf22ef7a516eb1ce3
LaymajouxClement <laymajouxclement@gmail.com>
2023-03-09 21:22:28 +0100

    fix client-side

--- client/js/dev/routeur.js
+++ client/js/dev/routeur.js
@@ -1,39 +1,49 @@
 export class Routeur{
     constructor(){
-        this.baseRouteur = "/client/html/";
+        this.baseRouteur = "/client/";
         this.queryString = window.location.search;
         if(this.queryString!=""){
             var urlParams = new URLSearchParams(this.queryString);
             try {
-                console.log(urlParams.get('routage'))
                 const product = urlParams.get('routage');
-                const file = product+".html";
-                this.includeHtmlFile(file);
+                const file = product;
+                this.includeHtmlFile(file,"html");
+                this.includeHtmlFile(file,"js","dev");
             } catch (error) {
-                alert("ah")
+                console.log(error)
             }
         }
     }
 
-    routerRequete(page){
-        return this.baseRouteur + page + "?t=" + new Date().getTime(); 
+    routerRequete(page,type,opt){
+        if(type=="js"){
+            return this.baseRouteur+type+"/"+opt+"/"+ page+"."+type + "?t=" + new Date().getTime(); 
+        }else{
+            return this.baseRouteur+type+"/"+ page + "?t=" + new Date().getTime(); 
+        }
     }
 
-    includeHtmlFile(page="menu.html",initFunc=false){
+    includeHtmlFile(page="menu.html",type="html",opt=false){
+        if(type!="html" && type!="js")return;
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', this.routerRequete(page), true);
-
-        xhr.responseType = 'document';
+        xhr.open('GET', this.routerRequete(page,type,opt), true);
+        
+        if(type=="html"){
+            xhr.responseType = 'document';
+        }
 
         // handle the response
         xhr.onload = function() {
         if (xhr.status === 200) {
-            // get the HTML document from the response
-            var htmlDocument = xhr.response.documentElement.innerHTML;
-            // insert the content into the page
-            document.getElementById('content').innerHTML = htmlDocument;
-            if(initFunc){
-                initFunc();
+            if(type=="html"){
+                // get the HTML document from the response
+                var htmlDocument = xhr.response.documentElement.innerHTML;
+                // insert the content into the page
+                document.getElementById('content').innerHTML = htmlDocument;
+            }else if(type=="js"){
+                var jsDocument = xhr.responseText;
+                // insert the content into the page
+                document.getElementById('script').innerHTML = jsDocument;
             }
         }
         };
